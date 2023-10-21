6c0205ca3f3b98b947771c6a06594dcddd286237
Aryan <aryan602.as@gmail.com>
2023-10-21 18:05:57 +0530

    added login page

---
+++ go-stream/src/pages/login.tsx
@@ -0,0 +1,10 @@
+
+export default function login() {
+  return (
+    <main className="bg-gradient-to-tr from-blue-500 via-purple-500 to-pink-500  h-screen bg-cover">
+        <div className="bg-white rounded-sm w-2/5 mx-auto">
+
+        </div>
+    </main>
+  )
+}
